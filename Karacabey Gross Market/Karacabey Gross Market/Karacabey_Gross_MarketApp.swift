326533055cf48bbea2667b883b91a673794bc107
aptus0 <temasre@tutanota.com>
2026-04-26 18:24:33 +0300

    feat: initialize iOS mobile application project with SwiftUI and CoreData

---
+++ Karacabey Gross Market/Karacabey Gross Market/Karacabey_Gross_MarketApp.swift
@@ -0,0 +1,21 @@
+//
+//  Karacabey_Gross_MarketApp.swift
+//  Karacabey Gross Market
+//
+//  Created by Samet on 24.04.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Karacabey_Gross_MarketApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
